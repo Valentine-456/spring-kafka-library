ec1ee15f09dd86e426f575f95165549bc45ffe00
Valentyn Bondarenko <murica.it@gmail.com>
2024-09-11 16:21:35 +0200

    Initial commit

---
+++ src/test/java/org/example/libraryproducer/LibraryProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.libraryproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
