fae5362099fc1616b6e79aa5590736a16d59555f
Valentyn Bondarenko <murica.it@gmail.com>
2024-09-12 17:07:41 +0200

    Add better unit+integration tests

--- src/test/java/integration/org/example/libraryproducer/controller/LibraryEventsControllerTest.java
+++ src/test/java/integration/org/example/libraryproducer/controller/LibraryEventsControllerTest.java
@@ -1,8 +1,14 @@
 package org.example.libraryproducer.controller;
 
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.shaded.io.opentelemetry.proto.trace.v1.Status;
 import org.example.libraryproducer.domain.LibraryEvent;
 import org.example.libraryproducer.util.TestUtil;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,14 +17,43 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EmbeddedKafka(topics = {"library-events"}, partitions = 3)
+@TestPropertySource(properties = {"spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}",
+        "spring.kafka.admin.properties.bootstrap.servers=${spring.embedded.kafka.brokers}"})
+
 class LibraryEventsControllerTest {
 
     @Autowired
     TestRestTemplate restTemplate;
+    @Autowired
+    EmbeddedKafkaBroker embeddedKafkaBroker;
+    private Consumer<Integer, String> consumer;
+
+    @BeforeEach
+    void setUp() {
+        var configs = new HashMap<>(KafkaTestUtils.consumerProps("group1", "true", embeddedKafkaBroker));
+        configs.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        consumer =
+                new DefaultKafkaConsumerFactory<>(configs, new IntegerDeserializer(), new StringDeserializer())
+                .createConsumer();
+        embeddedKafkaBroker.consumeFromAllEmbeddedTopics(consumer);
+    }
+
+    @AfterEach
+    void tearDown() {
+        consumer.close();
+    }
 
     @Test
     void postLibraryEvent() {
@@ -34,5 +69,8 @@ class LibraryEventsControllerTest {
         );
 
         assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+
+        var consumerRecords = KafkaTestUtils.getRecords(consumer);
+        assertEquals(1, consumerRecords.count());
     }
 }
---
+++ src/test/java/unit/org/example/libraryproducer/controller/LibraryEventsControllerUnitTest.java
@@ -0,0 +1,56 @@
+package org.example.libraryproducer.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.libraryproducer.domain.LibraryEvent;
+import org.example.libraryproducer.producer.LibraryEventsProducer;
+import org.example.libraryproducer.util.TestUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = LibraryEventsController.class)
+class LibraryEventsControllerUnitTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @MockBean
+    LibraryEventsProducer libraryEventsProducer;
+
+    @Test
+    void postLibraryEvent() throws Exception {
+        var payload  = objectMapper.writeValueAsString(TestUtil.libraryEventRecord());
+        when(libraryEventsProducer.sendLibraryEventWithHeaders(isA(LibraryEvent.class)))
+                .thenReturn(null);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/v1/library-event")
+                        .content(payload)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isCreated());
+    }
+
+    @Test
+    void postLibraryEventWithInvalidValues() throws Exception {
+        var payload  = objectMapper.writeValueAsString(TestUtil.bookRecordWithInvalidValues());
+        when(libraryEventsProducer.sendLibraryEventWithHeaders(isA(LibraryEvent.class)))
+                .thenReturn(null);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/v1/library-event")
+                        .content(payload)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isCreated());
+    }
+}
